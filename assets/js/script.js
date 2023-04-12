a77fd4c05fcf0ecdffe5e77af9ee3d6aa61e36a1
unknown <annieyashley@gmail.com>
2023-04-12 12:50:48 -0500

    adds event listener and redirects to second HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,11 @@ var searchButton = $("#submit-btn")
 
 function submitSearch(){
 
+    // grab values of the input form, for both the category and the inputted search
     var searchInputValue = searchInput.val();
     var searchTypeValue = searchType.val();
 
-    console.log(searchInputValue)
-    console.log(searchTypeValue)
-
+    // return errors if either category is blank
     if(!searchInputValue){
         alert("Please enter a value to search for.");
         return;
@@ -17,8 +16,16 @@ function submitSearch(){
         alert("Please select a search category.");
         return;
     }
+
+    // redirect to second wepage, append ?category = search, for example ?i=chicken
+    var URLdirect = "./search-results.html?" + searchTypeValue + "=" + searchInputValue
+
+    // actual redirect
+    location.assign(URLdirect)
 }
 
+
+// add event listener to redirect on click without reloading
 searchButton.on("click", function(e){
     e.preventDefault();
     submitSearch();
