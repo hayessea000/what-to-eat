138628b68e8b1e9f74df06bed064549244699658
Sean Hayes <hayessea000@outlook.com>
2023-04-18 13:14:30 -0500

    cleaned up code/files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,10 @@
 var searchInput = $("#searchinput")
 var searchType = $("#searchtype")
 var searchButton = $("#submit-btn")
-
 function submitSearch(){
-
     // grab values of the input form, for both the category and the inputted search
     var searchInputValue = searchInput.val();
     var searchTypeValue = searchType.val();
-
     // return errors if either category is blank
     if(!searchInputValue){
         alert("Please enter a value to search for.");
@@ -16,15 +13,11 @@ function submitSearch(){
         alert("Please select a search category.");
         return;
     }
-
     // redirect to second wepage, append ?category = search, for example ?i=chicken
     var URLdirect = "./search-results.html?" + searchTypeValue + "=" + searchInputValue
-
     // actual redirect
     location.assign(URLdirect)
 }
-
-
 // add event listener to redirect on click without reloading
 searchButton.on("click", function(e){
     e.preventDefault();
